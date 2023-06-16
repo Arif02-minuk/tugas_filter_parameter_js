33aa6a588b9d680353b6043d51536ded911bea4e
arif wijanarko <arifpethod@gmail.com>
2023-06-16 10:53:33 +0700

    tugas filter parameter javascript
    untuk niomic

---
+++ filterParameter.js
@@ -0,0 +1,18 @@
+function panggilParameters(value) {
+    var arr = [
+        { negara: 'indonesia', benua: 'Asia' },
+        { negara: 'Jerman', benua: 'Eropa' },
+        { negara: 'Spanyol', benua: 'Eropa' },
+        { negara: 'Korea', benua: 'Asia' },
+        { negara: 'Portugal', benua: 'Eropa' },
+        { negara: 'Amerika Serikat', benua: 'Amerika' },
+        
+    ]
+
+    var benuaEropa = arr.filter(function (item) {
+        return item.benua == value;
+    })
+
+    console.log(benuaEropa);
+}
+panggilParameters("Eropa");
